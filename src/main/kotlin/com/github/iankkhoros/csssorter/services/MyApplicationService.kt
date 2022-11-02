43feb8e424637d53d3bb966072daae10bc0847a4
GitHub Action <action@github.com>
2022-11-02 14:55:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/iankkhoros/csssorter/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/iankkhoros/csssorter/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.iankkhoros.csssorter.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iankkhoros.csssorter.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/iankkhoros/csssorter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/iankkhoros/csssorter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.iankkhoros.csssorter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iankkhoros.csssorter.MyBundle
 
 class MyProjectService(project: Project) {
 
